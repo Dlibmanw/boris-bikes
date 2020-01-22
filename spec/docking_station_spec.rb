c4e9c782d337c75f97a12795425f234ee28e6ac4
Student <student@Makerss-MacBook-Air-2.local>
2020-01-22 17:15:13 +0000

    Completing through chapter 10

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,9 @@
-require 'docking_station.rb'
-describe 'DockingStation' do
-  it 'return a bike' do
+require 'docking_station'
+describe DockingStation do
+  it {is_expected.to respond_to(:release_bike)}
+
+  it 'release working bikes' do
+    bike = subject.release_bike
+    expect(bike).to be_working
   end
 end
